a5d1df56832271de16e1abd50b262a3d35652c87
azertyminecraft <35696012+azertyminecraft@users.noreply.github.com>
2018-04-07 11:06:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -41,3 +41,19 @@ if(cmd === `${prefix}say`){
       message.reply("Tu n'es pas mon **créateur**.")
     }
   }
+    
+if (message.content.startsWith(prefix + 'embed')) {
+if (message.channel.type === "dm") return;
+if(!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) return message.reply("**:x: Vous n'avez pas la permission `manage-guild` dans ce serveur**").catch(console.error);
+    let args = message.content.split(" ").slice(1).join(" ");
+    if(!args) return message.channel.send("Vous voulez rien me faire répéter ?");
+    message.delete()
+    var embed = new Discord.RichEmbed();
+    embed.setColor(0xFF358B)
+      .setDescription(args)
+    message.channel.sendEmbed(
+      embed, {
+        disableEveryone: true
+      }
+    );
+  }}
