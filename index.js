8a7574b0d732dfb7bac6c17c37d78ef636c2265e
azertyminecraft <35696012+azertyminecraft@users.noreply.github.com>
2018-04-07 11:05:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,27 @@ bot.on('ready', () => {
 });
 
 bot.login(process.env.TOKEN);
+
+bot.on('message', message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  let sender = message.author;
+  let msg = message.content.toUpperCase();
+//Code
+
+if(cmd === `${prefix}say`){
+    if(message.author.id === '205752580251451392'){
+      let say = args.join(" ").slice();
+      message.delete(message.author)
+
+      message.channel.send(say)
+      console.log(`commande "say" demandé depuis le serveur ${message.guild.name}`)
+    } else {
+      message.reply("Tu n'es pas mon **créateur**.")
+    }
+  }
