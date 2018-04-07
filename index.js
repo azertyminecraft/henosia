7c8e3de9dcc664b9ffb2cb40215c0444b4aa3d3c
azertyminecraft <35696012+azertyminecraft@users.noreply.github.com>
2018-04-07 10:59:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ app.set('port', (process.env.PORT || 5000))
 app.listen(app.get('port'), function(){
     console.log(`bot en fonctionnement sur le port ${app.get('port')}`)
 })
+bot.on('ready', () => {
+    bot.user.setPresence({game: { name: 'Henosia - h!help', type: "WATCHING"}})
+    console.log("Bot Ready !");
+});
+
+bot.login(process.env.TOKEN);
